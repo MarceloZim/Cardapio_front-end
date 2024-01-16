29fac75a9666ec6e0e52722a21138d562d772cc0
MarceloZim <marcelod232015@gmail.com>
2024-01-15 23:30:50 -0300

    Criando useFoodDataMutate.ts / modal.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import './App.css'
 import { Card } from './components/card/card';
 import { useFoodData } from './components/hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
  const {data} = useFoodData();
+ const [isModalOpen, setIsModalOpen]= useState(false);
+
+ const handleOpenModal = () => {
+  setIsModalOpen(prev => !prev)
+ }
  
  return (
       <div className="container">
-        <h1>Cardápio\</h1>
+        <h1>Cardápio</h1>
         <div className="card-grid">
           {data?.map(foodData => 
           <Card 
@@ -16,6 +23,8 @@ function App() {
             image={foodData.image} 
           />)}
         </div>
+        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+        <button onClick={handleOpenModal}>Inserir</button>  
       </div>
    )
 }
